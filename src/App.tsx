1b9ce26711b4201cd6f73a72026462745c4fa33b
ColmiismaL <colruck32@gmail.com>
2023-09-26 17:23:38 +0900

    배배포 연습

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import './App';
 import { Routes, Route } from 'react-router-dom';
-import Mainpage from './page/main';
+import Mainpage from './page/main.tsx';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <Mainpage></Mainpage>
-    </div>
+    <GlobalStyle>
+      <Routes>
+        <Route path="/" element={<Mainpage />} />
+      </Routes>
+    </GlobalStyle>
   );
 }
 
